628f6bbb392b178c38ca225fbc4fedea8e073263
claaraa <clrstancu@gmail.com>
2020-05-12 09:37:04 +0300

    Changed the the icons from the RecyclerView

--- app/src/main/java/com/claramaria/gamesselector/MainActivity.java
+++ app/src/main/java/com/claramaria/gamesselector/MainActivity.java
@@ -15,7 +15,6 @@ import com.claramaria.gamesselector.activities.ListInteractiveGames;
 
 import java.util.ArrayList;
 
-
 public class MainActivity extends AppCompatActivity {
     private ArrayList<ListItemData> mList;
 
@@ -29,14 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void createList() {
         mList = new ArrayList<>();
-        mList.add(new ListItemData(R.drawable.ic_android, "Ball Games", "For this kind of games you will only need a ball"));
-        mList.add(new ListItemData(R.drawable.board_game, "Board Games", "If you have a board game, but not enough players, come here"));
-        mList.add(new ListItemData(R.drawable.drinking_games, "Drinking Games", "Are you 18+ and want to have some fun on a night?"));
-        mList.add(new ListItemData(R.drawable.deck_of_cards, "Card Games", "Some old fashion card games, but maybe you haven't heard of some of them. Wanna check them out?"));
-        mList.add(new ListItemData(R.drawable.hide_and_seek, "Interactive Games", "If you have some imagination an no items to play some games, please come here."));
+        mList.add(new ListItemData(R.drawable.ic_ball, "Ball Games", "For this kind of games you will only need a ball of any kind and some play mood."));
+        mList.add(new ListItemData(R.drawable.ic_cards, "Board Games", "If you have a board game, but not enough players, come here to search your team."));
+        mList.add(new ListItemData(R.drawable.ic_drink, "Drinking Games", "Are you 18+ and want to have some fun on a night with a some new people?"));
+        mList.add(new ListItemData(R.drawable.ic_cards, "Card Games", "What can you do with a deck of cards? Maybe some new friends! "));
+        mList.add(new ListItemData(R.drawable.ic_run, "Interactive Games", "If you have some imagination and no items to play some games, please come here."));
 
     }
 
