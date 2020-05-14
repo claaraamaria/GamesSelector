75a25faede3a0975b536c10c61713ba16313eea7
claaraa <clrstancu@gmail.com>
2020-05-14 15:44:07 +0300

    Navigation Drawer with fragments. Problems with fragments!

--- app/src/main/java/com/claramaria/gamesselector/MainActivity.java
+++ app/src/main/java/com/claramaria/gamesselector/MainActivity.java
@@ -2,8 +2,15 @@ package com.claramaria.gamesselector;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,10 +19,15 @@ import com.claramaria.gamesselector.activities.ListBoardGames;
 import com.claramaria.gamesselector.activities.ListCardGames;
 import com.claramaria.gamesselector.activities.ListDrinkingGames;
 import com.claramaria.gamesselector.activities.ListInteractiveGames;
+import com.claramaria.gamesselector.fragments.ChatFragment;
+import com.claramaria.gamesselector.fragments.MessageFragment;
+import com.claramaria.gamesselector.fragments.ProfileFragement;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
     private ArrayList<ListItemData> mList;
 
     @Override
@@ -26,6 +38,55 @@ public class MainActivity extends AppCompatActivity {
         createList();
         buildRecyclerView();
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_list);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_message:
+                        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new MessageFragment()).commit();
+                        break;
+                    case R.id.nav_chat:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChatFragment()).commit();
+                        break;
+                    case R.id.nav_profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragement()).commit();
+                        break;
+                    case R.id.nav_share:
+                        Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_send:
+                        Toast.makeText(MainActivity.this, "Send", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MessageFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_message);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     public void createList() {
